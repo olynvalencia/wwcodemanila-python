05dfa029f70606baf724681c5cd88a6b57d9d7a9
olyn <jonalyn.valencia@gmail.com>
2017-06-24 15:00:20 +0800

    python study group exercise 2

---
+++ exercise02.py
@@ -0,0 +1,35 @@
+""" Aling Nena's Reward System Challenge
+Author:
+Description: This summer, Aling Nena’s Sari-sari store wants to implement a
+reward system where customers can redeem a reward by texting the following:
+<Reward number 1-9> <Customer’s name> <Gender f or m>
+>> Please enter text: 1 nicole i. tibay f
+The system will then reply the following:
+Hi <Customer’s name first letters upper case>! You have successfully redeem
+reward #<reward number> - <reward>! Thank you for choosing Aling Nena’s
+Sari-sari store.
+>> Hi Nicole I. Tibay! You have successfully redeem reward #1 - Coke sakto!
+Thank you for choosing Aling Nena’s Sari-sari store.
+"""
+
+# You can access this by: rewards[<index>]
+# Just like strings the index starts with 0
+rewards = [
+    "Coke sakto",  # index 0
+    "Boy Bawang",  # index 1
+    "15pcs. Yucky candy",  # index 2
+    "15pcs. Pintura candy",  # index 3
+    "15PHP load",  # index 4
+    "3 pcs. Dove conditioner",  # index 5
+    "Cottonbuds",  # index 6
+    "One sheet of Biogesic",  # index 7
+    "100mL Pepper/Pintura",  # index 8
+]
+
+customer_input = input('Please enter text: ')
+customer_input_arr = customer_input.split( )
+
+name = ' '.join(customer_input_arr[1:len(customer_input_arr)-1])
+reward_index = int(customer_input_arr[0]) - 1
+print("Hi {}! You have successfully redeem reward #{} - {}! Thank you for choosing Aling Nena's Sari-sari store"
+    .format(name, customer_input_arr[0],rewards[reward_index]))
