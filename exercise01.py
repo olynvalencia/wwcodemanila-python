8e67379c2d472e8712d6b26ca46a58eecb6381e9
olyn <jonalyn.valencia@gmail.com>
2017-06-24 14:15:37 +0800

    added exercise header

--- exercise01.py
+++ exercise01.py
@@ -1,3 +1,11 @@
+""" The Challenge
+Author: Olyn Valencia
+Description: Aling Nena’s Sari-sari store wants a robot that will ask the
+customer their total bill and payment amount and tell them their change
+(for now, we can allow negative change).
+"""
+
+
 bill = input('How much is your total bill?')
 payment = input('How much is your payment?')
 change = float(payment) - float(bill)
